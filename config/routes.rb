c084f7e3e6ed74a1ef3ecbd60522c08fb75cf9b8
Leonardo de Assis <leosil@alunos.utfpr.edu.br>
2016-10-20 16:50:49 -0200

    Add static pages (home, about, and help) and its layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
 end
