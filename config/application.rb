e157598145968fba66b6e93d42eaedfaeabbd865
Guilherme Pontes <gui97p.p@gmail.com>
2016-10-26 23:28:37 -0200

    Activation email now working

--- config/application.rb
+++ config/application.rb
@@ -11,15 +11,5 @@ module ProjetoBd
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-       address:              'smtp.gmail.com',
-       port:                 587,
-       domain:               'gmail.com',
-       user_name:            'a17224app@gmail.com',
-       password:             'd?hkS+6fcM',
-       authentication:       'plain',
-       enable_starttls_auto: true
-    }
   end
 end
