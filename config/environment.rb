94283216083ba52c79a2f195ed5d36d704d47c8c
Guilherme Pontes <gui97p.p@gmail.com>
2016-10-26 23:43:28 -0200

    Activation email now working

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
-config.action_mailer.default_url_options = { :host => 'a17224.heroku.com' } #testing mailer with heroku
