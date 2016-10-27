c8d8c322a762eec1af2e17ab71ff11927df8fb39
Guilherme Pontes <gui97p.p@gmail.com>
2016-10-27 17:23:40 -0200

    email

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :area, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 200 }
-  
+
   self.per_page = 15
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
-  before_create :create_activation_digest
+  before_save :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -53,6 +53,11 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+      # Creates and assigns the activation token and digest.
+      def create_activation_digest
+        self.activation_token  = User.new_token
+        self.activation_digest = User.digest(activation_token)
+      end
   private
 
     # Converts email to all lower-case.
@@ -60,9 +65,4 @@ class User < ApplicationRecord
       self.email = email.downcase
     end
 
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
 end
