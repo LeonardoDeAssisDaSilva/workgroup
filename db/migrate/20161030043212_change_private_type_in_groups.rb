3b0985d72238d670342a98d508b6db796f968045
Leonardo de Assis <leosil@alunos.utfpr.edu.br>
2016-11-03 19:11:28 -0200

    Edit private integer to boolean migration

--- db/migrate/20161030043212_change_private_type_in_groups.rb
+++ db/migrate/20161030043212_change_private_type_in_groups.rb
@@ -1,5 +1,5 @@
 class ChangePrivateTypeInGroups < ActiveRecord::Migration[5.0]
   def change
-    change_column :groups, :private, :boolean
+    change_column :groups, :private, :boolean, 'boolean USING CAST(private AS boolean)'
   end
 end
