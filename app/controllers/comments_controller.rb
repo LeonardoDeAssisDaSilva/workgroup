ca8bf11b1031fc2797a60f78ee2cfed261ae0b12
Leonardo de Assis <leosil@alunos.utfpr.edu.br>
2016-11-18 21:16:43 -0200

    Finish functional comment section

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,16 @@ class CommentsController < ApplicationController
   def index
     @task = Task.find(params[:id])
     @comments = @task.comments.paginate(page: params[:page], :per_page => 15)
+
   end
-  
+
   def create
+    task = Task.find(params[:comment][:task_id])
     @comment = Comment.new(comment_params)
-    debugger
     if @comment.save
       flash[:success] = "Comentário submetido"
-      redirect_to task_comments_path(@comment.task.group.id, @comment.task.id)
+      redirect_to task_comments_path(task.group.id, task.id)
     else
-      task = Task.find(params[:comment][:task_id])
       flash[:danger] = "Erro ao submeter comentário"
       redirect_to group_task_path(task.group.id, task.id)
     end
@@ -39,9 +39,5 @@ class CommentsController < ApplicationController
     def comment_params
       params[:comment][:ancestry] = nil if params[:comment][:ancestry] == ""
       params.require(:comment).permit(:task_id, :user_id, :ancestry, :content)
-      
-#       params[:comment].delete_if { |key, value| value == "" }
-#       params[:comment][:ancestry] = nil
-      #params[:comment]
     end
 end
