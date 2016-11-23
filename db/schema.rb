ee0b0410074868f73663984adf9ddb131c55ffcf
Leonardo de Assis <leosil@alunos.utfpr.edu.br>
2016-11-23 19:25:29 -0200

    Update migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123002634) do
+ActiveRecord::Schema.define(version: 20161123200946) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -102,6 +102,8 @@ ActiveRecord::Schema.define(version: 20161123002634) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
